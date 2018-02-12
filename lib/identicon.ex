b6abde001dc5fd09051e23e63a90dfbb7e449486
cartrujillo <carlatrujillo93@gmail.com>
2018-02-12 21:26:48 +0100

    Primer commit con introduccion de texto

---
+++ lib/identicon.ex
@@ -0,0 +1,18 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for Identicon.
+  """
+
+  @doc """
+    main method
+
+  ## Examples
+
+      iex> Identicon.main("hola")
+      "Has dicho: hola"
+
+  """
+  def main(input) do
+    "Has dicho: #{input}"
+  end
+end
---
+++ test/identicon_test.exs
@@ -0,0 +1,9 @@
+defmodule IdenticonTest do
+  use ExUnit.Case
+  doctest Identicon
+
+"""  test "greets the world" do
+    assert Identicon.main("hola") == "Has dicho: hola"
+  end
+"""
+end
