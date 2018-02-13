d60f6b61340d1393ad8bd368663c4b72907aa872
cartrujillo <carlatrujillo93@gmail.com>
2018-02-13 17:47:28 +0100

    Functions build_grid and filter_odd added

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,18 +4,21 @@ defmodule Identicon do
   """
 
   @doc """
-    main method
-
   ## Examples
-      iex> Identicon.main("hola")
-      "Has dicho: hola"
+      iex> Identicon.main("adios")
+      "Has dicho: adios"
   """
   def main(input) do
     input
     |> hash_function
     |> pick_color
+    |> build_grid
+    |> filter_odd
   end
 
+  @doc """
+  Recibe el texto y genera una función hash
+  """
   def hash_function(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
@@ -26,4 +29,28 @@ defmodule Identicon do
   def pick_color(%Identicon.Image{hex: [r,g,b | _tail]} = image) do
     %Identicon.Image{image | color: {r,g,b}}
   end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
+  def filter_odd (%Identicon.Image{grid: grid} = image) do
+    grid =
+      Enum.filter(grid, fn({code, _index}) -> rem(code, 2) == 0 end)
+
+    %Identicon.Image{image | grid: grid}
+  end
+
 end
