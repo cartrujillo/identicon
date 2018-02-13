97ff93ada43b274af57985fe816f940e1b5b2dda
cartrujillo <carlatrujillo93@gmail.com>
2018-02-13 16:57:44 +0100

    Struct image added

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,12 +7,23 @@ defmodule Identicon do
     main method
 
   ## Examples
-
       iex> Identicon.main("hola")
       "Has dicho: hola"
-
   """
   def main(input) do
-    "Has dicho: #{input}"
+    input
+    |> hash_function
+    |> pick_color
+  end
+
+  def hash_function(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(%Identicon.Image{hex: [r,g,b | _tail]} = image) do
+    %Identicon.Image{image | color: {r,g,b}}
   end
 end
